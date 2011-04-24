eb39fd3810ca7c0a7f0f5036c9000d31b3e27db1
Mickael Daniel <daniel.mickael@gmail.com>
2011-04-24 18:02:25 +0200

    update server js to use lib/ext

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Just a basic server setup for this site
 var yabe = require('./lib/node-yabe'),
-config = require('./lib/config');
+config = require('./lib/config'),
+json = require('./lib/ext/json');
 
 yabe
-  .use(require('./lib/modules/json')( {foo: 'bar'} ))
+  .use(json( {foo: 'bar'} ))
   .listen(config.port);
 
 console.log('Node server is running! and listening on', config.port);
