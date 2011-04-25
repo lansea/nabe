8c637295c2b6029afb5cdb9b16837514a3d38961
Mickael Daniel <daniel.mickael@gmail.com>
2011-04-25 18:09:27 +0200

    a loot of stuff (pages, github project, ...)

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 // Just a basic server setup for this site
-var yabe = require('./lib/node-yabe'),
+var connect = require('connect'),
+yabe = require('./lib/node-yabe'),
 config = require('./lib/config'),
-json = require('./lib/ext/json');
+json = require('./lib/ext/json'),
+pages = require('./lib/ext/pages'),
+github = require('./lib/ext/github');
 
 yabe
+  
+  // these custom connects layer may be merged in node-yabe
   .use(json( {foo: 'bar'} ))
-  .listen(9599);
+  
+  .use(pages())
+  
+  .use(github())
+  
+  .use(connect.errorHandler({
+    stack: true,
+    message: true,
+    dump: true
+  }))
+  
+  .listen(config.port);
 
 console.log('Node server is running! and listening on', config.port);
