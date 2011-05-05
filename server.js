742e2927511094477c53ead000680d0eb79ead2c
Mickael Daniel <daniel.mickael@gmail.com>
2011-05-05 20:44:59 +0200

    merge in ext layers

--- server.js
+++ server.js
@@ -1,26 +1,7 @@
 // Just a basic server setup for this site
-var connect = require('connect'),
-yabe = require('./lib/node-yabe'),
-config = require('./lib/config'),
-json = require('./lib/ext/json'),
-pages = require('./lib/ext/pages'),
-github = require('./lib/ext/github');
+var nabe = require('./lib/node-yabe'),
+config = nabe.config;
 
-yabe
-  
-  // these custom connects layer may be merged in node-yabe
-  .use(pages())
-  
-  .use(github())
-  
-  .use(json( {foo: 'bar'} ))
-  
-  .use(connect.errorHandler({
-    stack: true,
-    message: true,
-    dump: true
-  }))
-  
-  .listen(config.port);
+nabe.listen(config.port);
 
 console.log('Node server is running! and listening on', config.port);
